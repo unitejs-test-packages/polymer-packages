3078127e3da6bb425e59f9bada582d78e9204946
Martyn Janes <martyn@obany.com>
2017-12-11 14:20:39 +0300

    Updated module paths

--- create-apps.sh
+++ create-apps.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-node unitejs/cli/bin/unite configure --packageName=polymer-javascript --title="Polymer JavaScript" --profile=PolymerJavaScript --outputDirectory=./apps/polymer-javascript
-node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-javascript
+node unitejs/unitejs-cli/bin/unite configure --packageName=polymer-javascript --title="Polymer JavaScript" --profile=PolymerJavaScript --outputDirectory=./apps/polymer-javascript
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-javascript
 
-node unitejs/cli/bin/unite configure --packageName=polymer-typescript-amd --title="Polymer TypeScript CommonJS" --profile=PolymerTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/polymer-typescript-commonjs
-node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-typescript-commonjs
+node unitejs/unitejs-cli/bin/unite configure --packageName=polymer-typescript-amd --title="Polymer TypeScript CommonJS" --profile=PolymerTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/polymer-typescript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-typescript-commonjs
 
-node unitejs/cli/bin/unite configure --packageName=polymer-typescript-systemjs --title="Polymer TypeScript SystemJS" --profile=PolymerTypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/polymer-typescript-systemjs
-node unitejs/cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-typescript-systemjs
+node unitejs/unitejs-cli/bin/unite configure --packageName=polymer-typescript-systemjs --title="Polymer TypeScript SystemJS" --profile=PolymerTypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/polymer-typescript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-typescript-systemjs
