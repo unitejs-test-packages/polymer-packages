3fdc7bf2343af6a80ba503765fd75d96eddfb095
Martyn Janes <martyn@obany.com>
2017-12-14 16:23:19 +0300

    Added E2E Tests

--- create-apps.sh
+++ create-apps.sh
@@ -2,13 +2,19 @@
 set -e
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=polymer-javascript --title="Polymer JavaScript" --profile=PolymerJavaScript --outputDirectory=./apps/polymer-javascript
+mkdir -p ./apps/polymer-javascript/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
+mkdir -p ./apps/polymer-javascript/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers-polymer/*.js $_
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/polymer-javascript
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-javascript
 
-node unitejs/unitejs-cli/bin/unite configure --packageName=polymer-typescript-amd --title="Polymer TypeScript CommonJS" --profile=PolymerTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/polymer-typescript-commonjs
+node unitejs/unitejs-cli/bin/unite configure --packageName=polymer-typescript-commonjs --title="Polymer TypeScript CommonJS" --profile=PolymerTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/polymer-typescript-commonjs
+mkdir -p ./apps/polymer-typescript-commonjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.ts $_
+mkdir -p ./apps/polymer-typescript-commonjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers-polymer/*.ts $_
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/polymer-typescript-commonjs
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-typescript-commonjs
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=polymer-typescript-systemjs --title="Polymer TypeScript SystemJS" --profile=PolymerTypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/polymer-typescript-systemjs
+mkdir -p ./apps/angular-typescript-systemjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.ts $_
+mkdir -p ./apps/angular-typescript-systemjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers-polymer/*.ts $_
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/polymer-typescript-systemjs
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-typescript-systemjs
