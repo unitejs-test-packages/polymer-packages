dcac1333f0ddf877347a2f62dd5ecdd1705a8474
Martyn Janes <martyn@obany.com>
2017-12-15 15:48:02 +0300

    Added rxjs

--- create-apps.sh
+++ create-apps.sh
@@ -6,6 +6,7 @@ mkdir -p ./apps/polymer-javascript/www/test/e2e/src/examples/ && cp -r ./unitejs
 mkdir -p ./apps/polymer-javascript/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers-polymer/*.js $_
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/polymer-javascript
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-javascript
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/polymer-javascript
 node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/polymer-javascript
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=polymer-typescript-commonjs --title="Polymer TypeScript CommonJS" --profile=PolymerTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/polymer-typescript-commonjs
@@ -13,6 +14,7 @@ mkdir -p ./apps/polymer-typescript-commonjs/www/test/e2e/src/examples/ && cp -r 
 mkdir -p ./apps/polymer-typescript-commonjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers-polymer/*.ts $_
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/polymer-typescript-commonjs
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-typescript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/polymer-typescript-commonjs
 node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/polymer-typescript-commonjs
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=polymer-typescript-systemjs --title="Polymer TypeScript SystemJS" --profile=PolymerTypeScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/polymer-typescript-systemjs
@@ -20,4 +22,5 @@ mkdir -p ./apps/polymer-typescript-systemjs/www/test/e2e/src/examples/ && cp -r 
 mkdir -p ./apps/polymer-typescript-systemjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers-polymer/*.ts $_
 node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/polymer-typescript-systemjs
 node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-typescript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/polymer-typescript-systemjs
 node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/polymer-typescript-systemjs
