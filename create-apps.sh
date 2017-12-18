76bf4113dd32ea5987ef21bd95f2e3ebe7a20567
Martyn Janes <martyn@obany.com>
2017-12-18 10:27:37 +0300

    Added JavaScript SystemJS Builds

--- create-apps.sh
+++ create-apps.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -e
 
-node unitejs/unitejs-cli/bin/unite configure --packageName=polymer-javascript --title="Polymer JavaScript" --profile=PolymerJavaScript --outputDirectory=./apps/polymer-javascript
-mkdir -p ./apps/polymer-javascript/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
-mkdir -p ./apps/polymer-javascript/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers-polymer/*.js $_
-node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/polymer-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/polymer-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/polymer-javascript
+node unitejs/unitejs-cli/bin/unite configure --packageName=polymer-javascript-commonjs --title="Polymer JavaScript CommonJS" --profile=PolymerJavaScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/polymer-javascript-commonjs
+mkdir -p ./apps/polymer-javascript-commonjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
+mkdir -p ./apps/polymer-javascript-commonjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers-polymer/*.js $_
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/polymer-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/polymer-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/polymer-javascript-commonjs
+
+node unitejs/unitejs-cli/bin/unite configure --packageName=polymer-javascript-systemjs --title="Polymer JavaScript SystemJS" --profile=PolymerJavaScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/polymer-javascript-systemjs
+mkdir -p ./apps/polymer-javascript-systemjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
+mkdir -p ./apps/polymer-javascript-systemjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers-polymer/*.js $_
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/polymer-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/polymer-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/polymer-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/polymer-javascript-systemjs
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=polymer-typescript-commonjs --title="Polymer TypeScript CommonJS" --profile=PolymerTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/polymer-typescript-commonjs
 mkdir -p ./apps/polymer-typescript-commonjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.ts $_
